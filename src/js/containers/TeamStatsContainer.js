70c815f9b37fd57952d606b3369d787710229951
markdyousef <mark@clai.io>
2017-01-27 16:18:43 +0100

    import all stats files and assigned them to data

--- src/js/containers/TeamStatsContainer.js
+++ src/js/containers/TeamStatsContainer.js
@@ -2,15 +2,32 @@
 import { connect } from 'react-redux';
 import BaseStats from '../components/BaseStats';
 import claiJson from '../../../../data/clai/slack_raw.json';
+import mastermindJson from '../../../../data/mastermind/slack_raw.json';
+import krispaJson from '../../../../data/krispa/slack_raw.json';
+import travelJson from '../../../../data/travel/slack_raw.json';
+import tradeX from '../../../../data/tradeX/slack_raw.json';
 
 const mapStateToProps = (state, ownProps) => {
     const props = {};
 
     const { team } = ownProps;
+    props.team = team;
     // select team
     switch (team) {
     case 'clai':
-        props.data = claiJson
+        props.data = claiJson;
+        break;
+    case 'mastermind':
+        props.data = mastermindJson;
+        break;
+    case 'krispa':
+        props.data = krispaJson;
+        break;
+    case 'travel':
+        props.data = travelJson;
+        break;
+    case 'tradeX':
+        props.data = tradeX;
         break;
     default:
         props.data = {};
