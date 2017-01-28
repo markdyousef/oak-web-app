9a1e9827ba982c5a149db731f9a0b28fb4c1982d
markdyousef <mark@clai.io>
2017-01-28 20:15:01 +0100

    styled message item and imported modules

--- src/js/components/MessageStats/MessageItem.js
+++ src/js/components/MessageStats/MessageItem.js
@@ -1,4 +1,6 @@
+// @flow
 import React, { PropTypes } from 'react';
+import moment from 'moment';
 
 import css from './MessageStats.css';
 
@@ -8,8 +10,11 @@ const MessageItem = ({ message }:Object) => {
             <div className={css.userAvatar}>
                 <span />
             </div>
-            <div>
+            <div className={css.text}>
                 <p>{message.text}</p>
+            </div>
+            <div className={css.time}>
+                {moment(message.ts * 1000).fromNow()}
             </div>
         </div>
     );
@@ -17,7 +22,9 @@ const MessageItem = ({ message }:Object) => {
 
 MessageItem.propTypes = {
     message: PropTypes.shape({
-        text: PropTypes.string.isRequired
+        text: PropTypes.string.isRequired,
+        user: PropTypes.string.isRequired,
+        ts: PropTypes.string.isRequired
     })
 }
 
