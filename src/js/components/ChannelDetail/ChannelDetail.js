5abded700d139feabf0733ca1b7b7072f4a07daf
markdyousef <mark@clai.io>
2017-01-28 10:13:07 +0100

    created ChannelDetail component

---
+++ src/js/components/ChannelDetail/ChannelDetail.js
@@ -0,0 +1,21 @@
+// @flow
+import React, { Component, PropTypes } from 'react';
+
+import css from './ChannelDetail.css';
+
+class ChannelDetail extends Component {
+    static propTypes = {};
+    constructor() {
+        super();
+        this.state = {};
+    }
+    render() {
+        return (
+            <div className={css.container}>
+                CHANNEL
+            </div>
+        );
+    }
+}
+
+export default ChannelDetail;
---
+++ src/js/components/ChannelDetail/ChannelDetail.test.js
@@ -0,0 +1,15 @@
+// @flow
+import React from 'react';
+import renderer from 'react-test-renderer';
+import ChannelDetail from './ChannelDetail';
+
+test('renders ChannelDetail', () => {
+    const props = {
+        name: 'general'
+    };
+    const component = renderer.create(
+        <ChannelDetail {...props} />
+    );
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+});
