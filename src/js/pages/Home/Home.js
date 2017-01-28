d5c40e93cd08876c28e803a8e142239a7db03b2a
markdyousef <mark@clai.io>
2017-01-28 21:29:53 +0100

    small style changes

--- src/js/pages/Home/Home.js
+++ src/js/pages/Home/Home.js
@@ -10,6 +10,9 @@ const Home = () => {
     return (
         <div className={css.container}>
             <TopNav />
+            <div className={css.toolBar}>
+
+            </div>
             <div className={css.teamStats}>
                 <TeamStats team="clai" />
             </div>
