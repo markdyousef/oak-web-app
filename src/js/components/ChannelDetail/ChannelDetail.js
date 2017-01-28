893e3373c056a07577165c76e5368a40f9cf7ca0
markdyousef <mark@clai.io>
2017-01-28 11:30:14 +0100

    added members display

--- src/js/components/ChannelDetail/ChannelDetail.js
+++ src/js/components/ChannelDetail/ChannelDetail.js
@@ -6,19 +6,34 @@ import css from './ChannelDetail.css';
 class ChannelDetail extends Component {
     static propTypes = {
         data: PropTypes.shape({
-            id: PropTypes.string.isRequired
+            id: PropTypes.string.isRequired,
+            name: PropTypes.string.isRequired,
+            members: PropTypes.arrayOf(PropTypes.string)
         })
     };
     constructor() {
         super();
+        this.renderMembers = this.renderMembers.bind(this);
         this.state = {};
+    }
+    renderMembers() {
+        const { members } = this.props.data;
+        return members.map(member => (
+            <div className={css.member} key={member}>
+                <span>{member}</span>
+            </div>
+        ))
     }
     render() {
         const { data } = this.props;
-        console.log(data);
         return (
             <div className={css.container}>
-                CHANNEL
+                <header>
+                    <h1>{data.name}</h1>
+                </header>
+                <div className={css.membersContainer}>
+                    {this.renderMembers()}
+                </div>
             </div>
         );
     }
