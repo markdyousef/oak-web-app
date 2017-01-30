3cd51b8372cdba1b174668fd80daa8cdfaf0ecdf
markdyousef <mark@clai.io>
2017-01-30 10:10:31 +0100

    added loader for images

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,18 +1,23 @@
 const path = require('path');
 
 module.exports = {
-  module: {
-    loaders: [
-      {
-        test: /\.css?$/,
-        loaders: [ 'style-loader', 'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]' ],
-        include: path.resolve(__dirname, '../')
-      },
-      {
-        test: /\.scss$/,
-        loader: [ 'style', 'css-loader', 'sass-loader' ],
-        include: path.resolve(__dirname, '../')
-      }
-    ]
-  }
-}
+    module: {
+        loaders: [
+            {
+                test: /\.css?$/,
+                loaders: ['style-loader', 'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]'],
+                include: path.resolve(__dirname, '../')
+            },
+            {
+                test: /\.scss$/,
+                loader: ['style', 'css-loader', 'sass-loader'],
+                include: path.resolve(__dirname, '../')
+            },
+            {
+        // inline base64 URLs for <=8k images, direct URLs for the rest
+                test: /\.(png|jpg)$/,
+                loader: 'url-loader?limit=8192'
+            }
+        ]
+    }
+};
