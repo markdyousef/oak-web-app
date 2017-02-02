ec3a0bf96b19cbcff520d03f3715ad5d3f9f81f8
markdyousef <mark@clai.io>
2017-02-02 19:05:56 +0100

    added members reducer

--- src/js/containers/TeamMembersContainer.js
+++ src/js/containers/TeamMembersContainer.js
@@ -1,10 +1,18 @@
 // @flow
 import { connect } from 'react-redux';
-
+import { getMembers } from '../actions';
 import TeamMembers from '../components/TeamMembers';
 
-const mapStateToProps = (state: Object) => ({});
+const mapStateToProps = (state: Object) => (
+    {
+        members: state.members.get('members')
+    }
+);
 
-const mapDispatchToProps = (dispatch: Function) => ({});
+const mapDispatchToProps = (dispatch: Function) => (
+    {
+        getMembers: (team: String) => dispatch(getMembers(team))
+    }
+);
 
 export default connect(mapStateToProps, mapDispatchToProps)(TeamMembers);
