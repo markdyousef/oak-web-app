b34f3a85f2ba8cdf267d355849eea2c52a72af26
markdyousef <mark@clai.io>
2017-02-02 19:09:35 +0100

    created members reducer

--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -3,9 +3,11 @@ import { combineReducers } from 'redux';
 import user from './user';
 import team from './team';
 import channel from './channel';
+import members from './members';
 
 export default combineReducers({
     user,
     team,
-    channel
+    channel,
+    members
 });
---
+++ src/js/reducers/members.js
@@ -0,0 +1,21 @@
+import Immutable from 'immutable';
+import * as type from '../constants/ActionTypes';
+
+const inititalState = Immutable.fromJS({
+    isLoading: true,
+    message: null,
+    team: 'clai',
+    members: Immutable.List([])
+});
+
+export default (state = inititalState, action) => {
+    switch (action.type) {
+    case type.RECEIVE_MEMBERS:
+        return state
+            .set('isLoading', false)
+            .set('team', action.team)
+            .set('members', Immutable.List(action.data));
+    default:
+        return state;
+    }
+};
