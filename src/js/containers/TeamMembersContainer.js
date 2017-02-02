9f405e4fac99f1c46a5b61afa92d4427ce763de5
markdyousef <mark@clai.io>
2017-02-02 18:50:20 +0100

    created team members container

---
+++ src/js/containers/TeamMembersContainer.js
@@ -0,0 +1,10 @@
+// @flow
+import { connect } from 'react-redux';
+
+import TeamMembers from '../components/TeamMembers';
+
+const mapStateToProps = (state: Object) => ({});
+
+const mapDispatchToProps = (dispatch: Function) => ({});
+
+export default connect(mapStateToProps, mapDispatchToProps)(TeamMembers);
