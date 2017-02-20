39d411f1d2ee7f49c116ca9cb20379c221f5cc73
markdyousef <mark@clai.io>
2017-02-20 16:20:57 +0100

    added basic styling to collection detail component

--- src/js/components/CollectionDetail/CollectionDetail.js
+++ src/js/components/CollectionDetail/CollectionDetail.js
@@ -1,6 +1,22 @@
 import React, { Component, PropTypes } from 'react';
+import Card from '../Card';
 
 import css from './CollectionDetail.css';
+
+const ITEMS = [
+    {
+        title: 'React',
+        id: '1'
+    },
+    {
+        title: 'Jest',
+        id: '2'
+    },
+    {
+        title: 'GraphQL',
+        id: '3'
+    }
+];
 
 class CollectionDetail extends Component {
     constructor() {
@@ -10,7 +26,12 @@ class CollectionDetail extends Component {
     render() {
         return (
             <div className={css.container}>
-
+                <div className={css.header}>
+                    TEXT
+                </div>
+                <div className={css.grid}>
+                    {ITEMS.map(item => <Card title={item.title} key={item.id} />)}
+                </div>
             </div>
         );
     }
