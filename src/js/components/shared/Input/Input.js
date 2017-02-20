21f1e61f8c8304a8468e1b65f6693f975a57b5be
markdyousef <mark@clai.io>
2017-02-20 18:24:17 +0100

    added type

--- src/js/components/shared/Input/Input.js
+++ src/js/components/shared/Input/Input.js
@@ -3,11 +3,12 @@ import React, { PropTypes } from 'react';
 
 import css from './Input.css';
 
-const Input = ({ title, value, placeholder, onChange }:Object) => {
+const Input = ({ title, value, placeholder, onChange, type }:Object) => {
     return (
         <div className={css.container}>
             <label htmlFor="input">{title}</label>
             <input
+                type={type}
                 value={value}
                 placeholder={placeholder}
                 onChange={event => onChange(event.target.value)}
@@ -20,12 +21,14 @@ Input.propTypes = {
     title: PropTypes.string.isRequired,
     value: PropTypes.string,
     placeholder: PropTypes.string,
-    onChange: PropTypes.func.isRequired
+    onChange: PropTypes.func.isRequired,
+    type: PropTypes.string
 };
 
 Input.defaultProps = {
     value: null,
-    placeholder: null
+    placeholder: null,
+    type: ''
 };
 
 export default Input;
