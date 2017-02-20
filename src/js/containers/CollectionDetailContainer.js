32da4d10dd9599b4ac3a250e8531423e7786c1aa
markdyousef <mark@clai.io>
2017-02-20 16:06:09 +0100

    added collection detail container

---
+++ src/js/containers/CollectionDetailContainer.js
@@ -0,0 +1,11 @@
+// @flow
+import { connect } from 'react-redux';
+import CollectionDetail from '../components/CollectionDetail';
+
+const mapStateToProps = (state: Object) => (
+    {}
+);
+
+const mapDispatchToProps = (dispatch: Function) => ({});
+
+export default connect(mapStateToProps, mapDispatchToProps)(CollectionDetail);
