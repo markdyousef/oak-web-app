a612607346b1e9fb3106249cfc69c15ff6f07955
markdyousef <mark@clai.io>
2017-02-21 09:42:19 +0100

    added color picker

--- src/js/components/LabelsActionBox/CreateLabels.js
+++ src/js/components/LabelsActionBox/CreateLabels.js
@@ -1,21 +1,49 @@
 import React, { Component, PropTypes } from 'react';
 import Input from '../shared/Input';
 
+const COLORS = [
+    '#0aa5ea',
+    '#f9b74b',
+    '#ea8465',
+    '#58c674',
+    '#b2668c'
+];
+
 import css from './LabelsActionBox.css';
 
 class CreateLabels extends Component {
     constructor() {
         super();
+        this.renderColorGrid = this.renderColorGrid.bind(this);
         this.state = {
-            name: ''
+            name: '',
+            selectedColor: COLORS[0]
         };
+    }
+    renderColorGrid() {
+        const { selectedColor } = this.state;
+        return (
+            <div className={css.colorChooser}>
+                {COLORS.map((color, index) => (
+                    <div
+                        key={index}
+                        style={{
+                            border: (color === selectedColor) ? '2px solid #303b42' : null,
+                            backgroundColor: color
+                        }}
+                        className={css.color}
+                        onClick={() => { this.setState({ selectedColor: color }); }}
+                    />
+                ))}
+            </div>
+        );
     }
     render() {
         const { name } = this.state;
         return (
             <div className={css.createContainer}>
                 <header>
-                    <h3>Create Label</h3>
+                    <h3>CREATE LABEL</h3>
                 </header>
                 <div className={css.name}>
                     <Input
@@ -23,6 +51,10 @@ class CreateLabels extends Component {
                         value={name}
                         onChange={value => this.setState({ name: value })}
                     />
+                </div>
+                <div className={css.selectColor}>
+                    <h5>SELECT COLOR: </h5>
+                    {this.renderColorGrid()}
                 </div>
             </div>
         );
