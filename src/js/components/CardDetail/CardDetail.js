37aba3f3a9b8fc8c5b1d67ed31895e27a5c38ead
markdyousef <mark@clai.io>
2017-02-21 10:33:02 +0100

    added mouseclick to close card

--- src/js/components/CardDetail/CardDetail.js
+++ src/js/components/CardDetail/CardDetail.js
@@ -9,8 +9,12 @@ import LabelsActionBox from '../LabelsActionBox';
 import css from './CardDetail.css';
 
 class CardDetail extends Component {
+    static propTypes = {
+        close: PropTypes.func.isRequired
+    }
     constructor() {
         super();
+        this.onMouseClick = this.onMouseClick.bind(this);
         this.state = {
             title: null,
             url: null,
@@ -18,39 +22,53 @@ class CardDetail extends Component {
             showLabels: false
         };
     }
+    componentDidMount() {
+        window.addEventListener('mousedown', this.onMouseClick, false);
+    }
+    componentWillUnmount() {
+        window.removeEventListener('mousedown', this.onMouseClick);
+    }
+    onMouseClick(event) {
+        const { close } = this.props;
+        if (event.path.filter(item => item.className === 'detail').length === 0) {
+            close();
+        }
+    }
     render() {
         const { close } = this.props;
         const { title, url, likes, showLabels } = this.state;
         return (
             <div className={css.modal}>
-                <div className={css.container}>
-                    <div className={css.close} onClick={() => close()}>X</div>
-                    <div className={css.main}>
-                        <div className={css.leftPane}>
-                            <div className={css.name}>
-                                <Input
-                                    title="TITLE"
-                                    onChange={(value: String) => this.setState({ title: value })}
-                                    value={title || ''}
+                <div className="detail">
+                    <div className={css.container}>
+                        <div className={css.close} onClick={() => close()}>X</div>
+                        <div className={css.main}>
+                            <div className={css.leftPane}>
+                                <div className={css.name}>
+                                    <Input
+                                        title="TITLE"
+                                        onChange={(value: String) => this.setState({ title: value })}
+                                        value={title || ''}
+                                    />
+                                    <h3>In collection <Link to="/">Design</Link></h3>
+                                </div>
+                                <div className={css.description}>
+                                    <Description />
+                                </div>
+                            </div>
+                            <div className={css.rightPane}>
+                                <Button
+                                    text="LIKE"
+                                    onClick={() => this.setState({ likes: likes + 1 })}
+                                    type="like"
+                                    value={likes}
                                 />
-                                <h3>In collection <Link to="/">Design</Link></h3>
+                                <Button
+                                    text="LABELS"
+                                    onClick={() => this.setState({ showLabels: !showLabels })}
+                                />
+                                {showLabels && <LabelsActionBox close={() => this.setState({ showLabels: false })} />}
                             </div>
-                            <div className={css.description}>
-                                <Description />
-                            </div>
-                        </div>
-                        <div className={css.rightPane}>
-                            <Button
-                                text="LIKE"
-                                onClick={() => this.setState({ likes: likes + 1 })}
-                                type="like"
-                                value={likes}
-                            />
-                            <Button
-                                text="LABELS"
-                                onClick={() => this.setState({ showLabels: !showLabels })}
-                            />
-                            {showLabels && <LabelsActionBox close={() => this.setState({ showLabels: false })} />}
                         </div>
                     </div>
                 </div>
