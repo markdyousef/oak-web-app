4c4553d8ea2be3f3b645c29983ad7dcb87703664
markdyousef <mark@clai.io>
2017-02-21 09:32:13 +0100

    added name input for create labels

--- src/js/components/LabelsActionBox/CreateLabels.js
+++ src/js/components/LabelsActionBox/CreateLabels.js
@@ -1,18 +1,29 @@
 import React, { Component, PropTypes } from 'react';
+import Input from '../shared/Input';
 
 import css from './LabelsActionBox.css';
 
 class CreateLabels extends Component {
     constructor() {
         super();
-        this.state = {};
+        this.state = {
+            name: ''
+        };
     }
     render() {
+        const { name } = this.state;
         return (
             <div className={css.createContainer}>
                 <header>
                     <h3>Create Label</h3>
                 </header>
+                <div className={css.name}>
+                    <Input
+                        title="NAME"
+                        value={name}
+                        onChange={value => this.setState({ name: value })}
+                    />
+                </div>
             </div>
         );
     }
