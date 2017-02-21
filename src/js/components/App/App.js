a8beda1df769b956a2b18925421eb78cbb95411d
markdyousef <mark@clai.io>
2017-02-21 17:18:15 +0100

    added top nav

--- src/js/components/App/App.js
+++ src/js/components/App/App.js
@@ -1,4 +1,5 @@
 import React, { Component, PropTypes } from 'react';
+import TopNav from '../TopNav';
 
 import css from './App.css';
 
@@ -16,6 +17,7 @@ class App extends Component {
     render() {
         return (
             <div className={css.container}>
+                <TopNav />
                 {this.props.children}
             </div>
         );
--- src/js/components/TopNav/TopNav.js
+++ src/js/components/TopNav/TopNav.js
@@ -2,9 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router';
 
 import css from './TopNav.css';
-import logo from '../../../img/full_blue.png';
 import Avatar from '../shared/Avatar';
-import teamIcon from '../../../img/multiple-users-silhouette.png';
 
 const IMG = '//style.anu.edu.au/_anu/4/images/placeholders/person.png'
 class TopNav extends Component {
@@ -16,13 +14,9 @@ class TopNav extends Component {
         return (
             <nav className={css.container}>
                 <div className={css.navLeft}>
-                    <img src={logo} alt="logo" />
                 </div>
                 <div className={css.navRight}>
-                    <Link to="/" activeClassName={css.active}>
-                        TEAMS
-                    </Link>
-                    <Link to="/me" className={css.myProfile}>
+                    <Link to="/" className={css.myProfile}>
                         <Avatar img={IMG} />
                     </Link>
                 </div>
