8268b00b4520bb5177179a65dbb30e5cfdec9837
markdyousef <mark@clai.io>
2017-02-22 16:37:18 +0100

    added signup container

--- src/js/components/Login/Login.js
+++ src/js/components/Login/Login.js
@@ -12,7 +12,7 @@ class Login extends Component {
         this.state = {
             email: '',
             password: ''
-        }
+        };
     }
     render() {
         const { email, password } = this.state;
