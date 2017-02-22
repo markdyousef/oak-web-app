b96106ab88be5f8ff8e1edaee2ad9ede4b15f6cb
markdyousef <mark@clai.io>
2017-02-22 16:33:56 +0100

    added inputs, buttons and styling to login

--- src/js/components/Login/Login.js
+++ src/js/components/Login/Login.js
@@ -1,5 +1,7 @@
 // @flow
 import React, { Component, PropTypes } from 'react';
+import Input from '../shared/Input';
+import Button from '../shared/Button';
 
 import css from './Login.css';
 
@@ -7,13 +9,43 @@ class Login extends Component {
     static propTypes = {};
     constructor() {
         super();
-        this.state = {}
+        this.state = {
+            email: '',
+            password: ''
+        }
     }
     render() {
-        console.log('login');
+        const { email, password } = this.state;
         return (
             <div className={css.container}>
-                LOGIN
+                <div className={css.login}>
+                    <div className={css.inputs}>
+                        <Input
+                            title="Email"
+                            value={email}
+                            onChange={value => this.setState({ email: value })}
+                            type="email"
+                        />
+                        <Input
+                            title="Password"
+                            value={password}
+                            onChange={value => this.setState({ password: value })}
+                            type="password"
+                        />
+                    </div>
+                    <div className={css.buttons}>
+                        <Button
+                            onClick={() => {}}
+                            text="LOGIN"
+                            type="primary"
+                        />
+                        <Button
+                            onClick={() => {}}
+                            text="CREATE"
+                            // type="transparent"
+                        />
+                    </div>
+                </div>
             </div>
         );
     }
