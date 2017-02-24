00a03297920701a1096e475a8ee227f212b3a0a4
markdyousef <mark@clai.io>
2017-02-24 14:54:05 +0100

    added create collection mutation

--- src/js/components/CollectionDialog/CollectionDialog.js
+++ src/js/components/CollectionDialog/CollectionDialog.js
@@ -6,14 +6,24 @@ import css from './CollectionDialog.css';
 
 class CollectionDialog extends Component {
     static propTypes = {
-        close: PropTypes.func.isRequired
+        close: PropTypes.func.isRequired,
+        create: PropTypes.func.isRequired
     }
     static defaultProps = {}
     constructor() {
         super();
+        this.onSave = this.onSave.bind(this);
         this.state = {
-            name: ''
+            name: '',
+            message: ''
         };
+    }
+    onSave() {
+        const { name } = this.state;
+        const { create, close } = this.props;
+        create(name)
+            .then(() => close())
+            .catch(err => console.log(err))
     }
     render() {
         const { close } = this.props;
@@ -38,7 +48,7 @@ class CollectionDialog extends Component {
                             text="Cancel"
                         />
                         <Button
-                            onClick={close}
+                            onClick={this.onSave}
                             text="Save"
                             type="primary"
                         />
---
+++ src/js/containers/CollectionDialogContainer.js
@@ -0,0 +1,18 @@
+// @flow
+import { graphql, compose } from 'react-apollo';
+import gql from 'graphql-tag';
+import CollectionDialog from '../components/CollectionDialog';
+
+const createGrove = gql`
+    mutation createGrove($title: String!) {
+        createGrove(title: $title)
+    }
+`;
+
+export default compose(
+    graphql(createGrove, {
+        props: ({ mutate }) => ({
+            create: title => mutate({ variables: { title } })
+        })
+    })
+)(CollectionDialog);
