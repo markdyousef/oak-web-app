35238a7b123cdfe9b80ac6b979790a95007cd167
markdyousef <mark@clai.io>
2017-03-31 10:58:47 +0200

    added svg icons to card

--- src/js/components/Card/Card.js
+++ src/js/components/Card/Card.js
@@ -60,9 +60,8 @@ const Bottom = styled.div`
     width: 100%;
     display: flex;
     justify-content: space-between;
-    & button {
-        display: inline-block;
-        ${button()}
+    & div {
+        display: flex;
     }
 `;
 
@@ -71,12 +70,20 @@ const Time = styled.span`
     color: ${colors.grey}
 `;
 
-const Icon = styled.button`
+const Icon = styled.div`
+    height: 18px;
     display: flex;
     align-items: center;
-    margin-right: 10px;
+    margin-right: 15px;
+    cursor: pointer;
     & span {
-        color: ${colors.grey}
+        color: ${colors.grey};
+        font-size: 14px;
+    }
+    & svg {
+        height: 100%;
+        width: 18px;
+        margin-right: 3px;
     }
 `;
 
@@ -133,18 +140,72 @@ export default class Card extends Component {
         );
     }
     renderBottom = () => {
-        const { updatedAt, labels, comments, onShow } = this.props;
+        const { updatedAt, comments, onShow } = this.props;
         return (
             <Bottom>
                 <div>
                     <Icon>
+                        <svg
+                            version="1.1"
+                            id="Layer_1"
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 16.6 16"
+                            style={{ enableBackground: 'new 0 0 16.6 16' }}
+                        >
+                            <g>
+                                <path
+                                    d="M14.3,16L14.3,16c-1.3,0-2.7-0.8-3.2-1.2c-0.9,0.3-1.8,0.5-2.8,0.5c-4.5,0-8.1-3.4-8.1-7.5s3.6-7.5,8.1-7.5
+                                    s8.1,3.4,8.1,7.5c0,1.5-0.5,2.9-1.3,4.1c0.5,0.6,1.3,2,1.1,3c-0.1,0.4-0.4,0.7-0.8,0.9C15,15.9,14.7,16,14.3,16z M11.2,13.7
+                                    l0.2,0.2c0,0,1.6,1.1,2.9,1.1l0,0c0.2,0,0.4,0,0.6-0.1c0.2-0.1,0.2-0.2,0.2-0.2c0.1-0.5-0.6-1.7-1.1-2.4L13.7,12l0.2-0.3
+                                    c0.9-1.1,1.3-2.4,1.3-3.8c0-3.6-3.2-6.5-7.1-6.5S1.2,4.2,1.2,7.8s3.2,6.5,7.1,6.5c0.9,0,1.8-0.2,2.7-0.5L11.2,13.7z"
+                                />
+                            </g>
+                        </svg>
                         <span>{comments.length}</span>
                     </Icon>
                     <Icon>
-                        <span>{labels.length}</span>
+                        <svg
+                            version="1.1"
+                            id="Layer_1"
+                            viewBox="0 0 17.8 16"
+                            style={{ enableBackground: 'new 0 0 17.8 16' }}
+                        >
+                            <g id="XMLID_2_">
+                                <g>
+                                    <g>
+                                        <path
+                                            d="M8.9,15.6c-0.1,0-0.2,0-0.3-0.1c-0.8-0.5-1.4-1-2.1-1.4C3.4,12,2,11.1,0.7,8.2c-1.6-3.5,0.4-5.8,0.5-5.9
+                                                c0,0,1.6-2.1,4.1-1.9c1.8,0.2,3,1,3.6,1.5c0.6-0.5,1.8-1.4,3.6-1.5c2.6-0.2,4.1,1.8,4.2,1.9s2,2.3,0.4,5.8
+                                                C15.8,11,14.3,12,11.3,14c-0.6,0.4-1.3,0.9-2.1,1.4C9.1,15.5,9,15.6,8.9,15.6z M4.9,1.4C3.1,1.4,2,3,1.9,3
+                                                C1.8,3.1,0.3,4.9,1.6,7.8c1.2,2.6,2.4,3.4,5.5,5.5c0.5,0.4,1.1,0.8,1.8,1.2c0.7-0.5,1.3-0.9,1.8-1.2c3-2,4.3-2.8,5.4-5.4
+                                                c1.3-3-0.3-4.8-0.3-4.8c0-0.1-1.3-1.7-3.3-1.6C10.5,1.6,9.3,3,9.3,3C9.2,3.1,9,3.2,8.9,3.2C8.7,3.1,8.6,3.1,8.5,3
+                                                S7.2,1.6,5.2,1.5C5.1,1.4,5,1.4,4.9,1.4z"
+                                        />
+                                    </g>
+                                </g>
+                            </g>
+                        </svg>
+                        <span>{0}</span>
                     </Icon>
                     <Icon onClick={onShow}>
-                        Ex
+                        <svg
+                            version="1.1"
+                            id="Layer_1"
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 16 16"
+                            style={{ enableBackground: 'new 0 0 16 16' }}
+                        >
+                            <g>
+                                <path
+                                    d="M6.9,8.4l-5.6,5.6v-3.7c0-0.3-0.2-0.5-0.5-0.5s-0.4,0.2-0.4,0.5v5.1l0.3,0.3h5.1c0.2,0,0.4-0.2,0.4-0.5
+                                    c0-0.3-0.2-0.5-0.4-0.5H2l5.5-5.5c0.2-0.2,0.2-0.5,0-0.6C7.4,8.4,7.2,8.2,6.9,8.4z"
+                                />
+                                <path
+                                    d="M15.2,0.5H10l0,0c0,0-0.1,0.2-0.1,0.4c0,0.3,0.2,0.5,0.5,0.5H14L8.4,7c-0.2,0.2-0.2,0.5,0,0.6c0.1,0.1,0.2,0.1,0.3,0.1
+                                    c0.1,0,0.2,0,0.3-0.1l5.5-5.5v3.7c0,0.3,0.2,0.5,0.5,0.5s0.5-0.2,0.5-0.5V0.7L15.2,0.5z"
+                                />
+                            </g>
+                        </svg>
                     </Icon>
                 </div>
                 <Time>
